eb770c229059fec5dc2f8a472d86f39ac11c0abe
Alex Chrome <dify.chrome@gmail.com>
2012-02-27 12:22:30 +0700

    added auto-watch for new issues and ex-assignee

--- lib/auto_watchers/hooks/auto_watchers_issue_hook.rb
+++ lib/auto_watchers/hooks/auto_watchers_issue_hook.rb
@@ -2,6 +2,10 @@
 # saved.
 class AutoWatchersIssuesControllerHook < Redmine::Hook::ViewListener
 
+  def controller_issues_new_before_save(context={})
+    controller_issues_edit_before_save context
+  end
+
   def controller_issues_edit_before_save(context={})
     configuration = Setting.plugin_redmine_auto_watchers
     if configuration['enable_watchers']
@@ -11,6 +15,14 @@ class AutoWatchersIssuesControllerHook < Redmine::Hook::ViewListener
       if !issue.assigned_to.blank? and 
          issue.addable_watcher_users.include?(issue.assigned_to)
           issue.add_watcher(issue.assigned_to)
+      end
+      # Add ex-assignee to watchers if they are exists and addable
+      unless issue.new_record?
+        current_issue = Issue.find issue.id
+        if !current_issue.assigned_to.blank? and
+           issue.addable_watcher_users.include?(current_issue.assigned_to)
+            issue.add_watcher(current_issue.assigned_to)
+        end
       end
       # Add the current user if they are addable
       issue.add_watcher(User.current) if issue.addable_watcher_users.include?(User.current.id)
